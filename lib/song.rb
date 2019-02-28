0bbf75b90c667ca1daad0b702470d84031bdcf87
Mackenzie Moore <macmoore@ucdavis.edu>
2019-02-28 21:16:34 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,9 @@
 class Song 
-  attr_accessor :name, :artist, :genre 
+  attr_reader :name, :artist, :genre 
 @@song_count = 0 
-def initialize
+def initialize(name, artist, genre)
   @@song_count += 1 
-
+  @name = name 
+  @artist = artist
+  @genre = genre 
 end 
